1ca6718b83e5d4188f351e6a46c29d78dc8be48e
Navi <navisenthilnadhan@gmail.com>
2024-05-27 14:46:29 +0530

    Final check

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-  baseURL: 'http://localhost:5000',
-  // baseURL: 'https://kube.onrender.com',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://kube.onrender.com',
   timeout: 5000, 
   headers: {
     'Content-Type': 'application/json',
